7e77ebfe7c0d4a18d9ce88566870236ff4d59b76
Ahmed Iqbal Bhatti <ahmad786687iqbal@gmail.com>
2023-10-03 21:58:38 +0500

    Created Reusable Rating Component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import StarRating from "./StarRating";
+// import './index.css';
+// import App from './App';
+
+function Test() {
+    const [movieRating, setMovieRating] = useState(0 );
+    const handleMovieRating = (rating)=>{
+        setMovieRating(rating);
+    }
+    return (
+        <div>
+            <StarRating maxRating={8} color={"blue"} onRatingChange={handleMovieRating}/>
+            <p>This movie was rated {movieRating} Stars</p>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        {/*<App />*/}
+        <StarRating maxRating={5} message={[
+            "Terrible", "Bad", "Okay", "Good", "Amazing"
+        ]} defaultRating={5}/>
+        <StarRating size={24} color={"red"} maxRating={5} className="test"/>
+        <Test/>
+
+    </React.StrictMode>
 );
